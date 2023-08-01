e7ee81af95cb24e6d9205e3b0ecbdf2522dd897b
Danil Neklyudov <dejeksdan@gmail.com>
2023-08-01 18:50:24 +0500

    Task4 completed

---
+++ Task4.java
@@ -0,0 +1,24 @@
+package org.example.Homework.HWException;
+
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Введите строку: ");
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                throw new Exception("Пустые строки вводить нельзя.");
+            }
+
+            System.out.println("Вы ввели: " + input);
+
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
